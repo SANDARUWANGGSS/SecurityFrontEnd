a2a4ff953f8e2550ed9268210ad3b48eaa2e988b
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-10-02 14:31:16 +0530

    auth headers completed

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,11 +6,16 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
 
-  BASE_URL = "http://localhost:8081"
+  BASE_URL = "http://localhost:8081";
+
+  requestHeader = new HttpHeaders(
+    {"No-Auth":"True"}
+  )
+
   constructor(private httpClient:HttpClient) { }
 
   public login(loginData:any)
   {
-    return this.httpClient.post(this.BASE_URL+"/authentication",loginData)
+    return this.httpClient.post(this.BASE_URL+"/authentication",loginData,{headers:this.requestHeader})
   }
 }
