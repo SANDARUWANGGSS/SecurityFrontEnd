297db34f4101c91feae00674711ce8f8c1e47ed1
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-10-03 17:56:32 +0530

    Added routing

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserAuthService } from 'src/app/service/user-auth.service';
 import { UserService } from 'src/app/service/user.service';
 
 @Component({
@@ -8,7 +10,11 @@ import { UserService } from 'src/app/service/user.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit{
-  constructor(private userService:UserService)
+  constructor(
+    private userService:UserService,
+    private userAuthService:UserAuthService,
+    private router:Router
+    )
   {
 
   }
@@ -19,11 +25,24 @@ export class LoginComponent implements OnInit{
 
   login(loginForm:NgForm)
   {
-    console.log("Form is submitted");
-    console.log(loginForm.value);
+    // console.log("Form is submitted");
+    // console.log(loginForm.value);
     this.userService.login(loginForm.value).subscribe(
-      (response)=>{
-        console.log(response);
+      (response:any)=>{
+        console.log(response.jwtToken);
+        console.log(response.user.role);
+        this.userAuthService.setRoles(response.user.role);
+        this.userAuthService.setToken(response.jwtToken);
+
+        const role = response.user.role[0].roleName;
+        if(role === 'Admin')
+        {
+          this.router.navigate(['/admin'])
+        }
+        else
+        {
+          this.router.navigate(['/user'])
+        }
       },
       (error=>{
         console.log(error)
