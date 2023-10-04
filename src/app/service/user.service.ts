0a8256f3cfe7e04a5ecd2835d5772444972e9de9
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-10-04 09:47:34 +0530

    Changed user service

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -3,52 +3,36 @@ import { Injectable } from '@angular/core';
 import { UserAuthService } from './user-auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  BASE_URL = 'http://localhost:8081';
 
-  BASE_URL = "http://localhost:8081";
-
-  requestHeader = new HttpHeaders(
-    {"No-Auth":"True"}
-  )
+  requestHeader = new HttpHeaders({ 'No-Auth': 'True' });
 
   constructor(
-    private httpClient:HttpClient,
-    private userAuthService:UserAuthService
-    ) { }
+    private httpClient: HttpClient,
+    private userAuthService: UserAuthService
+  ) {}
 
-  public login(loginData:any)
-  {
-    return this.httpClient.post(this.BASE_URL+"/authentication",loginData,{headers:this.requestHeader})
+  public login(loginData: any) {
+    return this.httpClient.post(this.BASE_URL + '/authentication', loginData, {
+      headers: this.requestHeader,
+    });
   }
 
-  public roleEqual(allowRoles: any): boolean
-  {
-    if (allowRoles) {
-      // Handle the case where allowRoles is defined
-      return true;  // or return some other boolean value based on your logic
-    } else {
-      // Handle the case where allowRoles is undefined or null
-      return false;  // or return some other boolean value based on your logic
-    }
-    
+  // @ts-ignore
+  public roleEqual(allowRoles): boolean {
     let isMatch = false;
-    const userRoles:any = this.userAuthService.getRoles();
+    const userRoles: any = this.userAuthService.getRoles();
 
-    if(userRoles != null && userRoles)
-    {
-      for(let i = 0; i < userRoles.length; i++)
-      {
-        for(let j =0; j < allowRoles.length; j++)
-        {
-          if(userRoles[i].roleName === allowRoles[j])
-          {
+    if (userRoles != null && userRoles) {
+      for (let i = 0; i < userRoles.length; i++) {
+        for (let j = 0; j < allowRoles.length; j++) {
+          if (userRoles[i].roleName === allowRoles[j]) {
             isMatch = true;
             return isMatch;
-          }
-          else
-          {
+          } else {
             return isMatch;
           }
         }
